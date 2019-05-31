771019096da51b8f0bd1a378d7c4e79612b51362
Seth Rah <seth.evangelion.rah@gmail.com>
2019-05-31 18:03:21 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# docker-compose-autoinstall
+Streamline the process of installing docker with docker-compose on ubuntu servers.
+
+Just run the following command in your ubuntu server to download the script
+
+`wget https://raw.githubusercontent.com/seth-rah/docker-compose-autoinstall/master/install.sh` 
+
+Once downloaded run `sh ./install.sh` as root
---
+++ install.sh
@@ -0,0 +1,36 @@
+if [ "$(id -un)" != "root" ]
+        then echo "Please run this script as sudo before continuing"
+        exit
+fi
+echo "please enter the name of the ubuntu user that will be running docker commands on this server"
+read line
+if id "$line" >/dev/null 2>&1; then
+        echo "user exists, continuing"
+else
+        echo "user does not exist, stopping"
+		exit
+fi
+apt-get purge lxc-docker
+apt-get remove docker-engine
+apt-get purge docker-engine
+apt-get -y install apt-transport-https ca-certificates curl
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+apt-get update
+apt-get -y install docker-ce
+service docker start
+usermod -aG docker $line
+curl -L "https://github.com/docker/compose/releases/download/1.22.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo "============================================================================================================"
+echo "Please log out of this session and then log in again"
+echo "Run 'docker login url.to.private.registry.here' when you log in again, and fill in your registry credentials"
+echo "============================================================================================================"
+echo " "
+echo " "
