3835a89e4fd23f038a78b08335c90f7f80822f54
Seth Rah <seth.evangelion.rah@gmail.com>
2019-05-31 18:10:43 +0200

    Adjusted script to use latest version of docker-compose and improved readme

--- README.md
+++ README.md
@@ -6,3 +6,11 @@ Just run the following command in your ubuntu server to download the script
 `wget https://raw.githubusercontent.com/seth-rah/docker-compose-autoinstall/master/install.sh` 
 
 Once downloaded run `sh ./install.sh` as root
+
+## Features
+
+1. Remove / purge depreciated docker versions
+2. Install docker-ce
+3. Install docker-compose
+4. Give provided user privileges to execute docker commands
+5. Give docker-compose execution rights
--- install.sh
+++ install.sh
@@ -10,6 +10,7 @@ else
         echo "user does not exist, stopping"
 		exit
 fi
+apt-get remove lxc-docker
 apt-get purge lxc-docker
 apt-get remove docker-engine
 apt-get purge docker-engine
@@ -20,7 +21,7 @@ apt-get update
 apt-get -y install docker-ce
 service docker start
 usermod -aG docker $line
-curl -L "https://github.com/docker/compose/releases/download/1.22.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+curl -L "https://github.com/docker/compose/releases/download/1.24.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
 echo " "
 echo " "
